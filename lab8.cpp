735287331b698a238874e7172e376c3983ff5831
Sobitora <88590988+Sobitora@users.noreply.github.com>
2021-09-27 23:11:28 +0700

    Add files via upload

---
+++ lab8.cpp
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#define scount 80
+#define screen_x 80
+#define screen_y 25
+
+HANDLE wHnd;
+HANDLE rHnd;
+DWORD fdwMode;
+CHAR_INFO consoleBuffer[screen_x * screen_y];
+COORD bufferSize = { screen_x,screen_y };
+COORD characterPos = { 0,0 };
+SMALL_RECT windowSize = { 0,0,screen_x-1,screen_y-1 };
+COORD star[scount];
+int HP=10;
+int setConsole(int x, int y)
+{
+wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
+SetConsoleScreenBufferSize(wHnd, bufferSize);
+return 0;
+}
+
+int setMode()
+{
+rHnd = GetStdHandle(STD_INPUT_HANDLE);
+fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+SetConsoleMode(rHnd, fdwMode);
+return 0;
+}
+
+void clear_buffer()
+{
+for (int y = 0; y < screen_y; ++y) {
+    for (int x = 0; x < screen_x; ++x) {
+        consoleBuffer[x + screen_x * y].Char.AsciiChar = ' ';
+        consoleBuffer[x + screen_x * y].Attributes = 7;
+    }
+}
+}
+
+void player_buffer(int x,int y,int color)
+{
+    char ship[6] = "<-0->";
+    for (int i = 0; i < 5; i++) {
+        consoleBuffer[(x+i) + screen_x * y].Char.AsciiChar = ship[i];
+        consoleBuffer[(x+i) + screen_x * y].Attributes = color;
+    }
+}
+
+void fill_buffer_to_console()
+{
+WriteConsoleOutputA(wHnd, consoleBuffer, bufferSize, characterPos,&windowSize);
+}
+
+void init_star()
+{
+for (int i = 0; i < scount; i++) {
+    star[i].X=(rand() % screen_x);
+    star[i].Y=(rand() % screen_y);
+}
+}
+void star_fall()
+{
+    int i;
+    for (i = 0; i < scount; i++) {
+        if (star[i].Y >= screen_y-1) {
+        star[i] = { (short)(rand() % screen_x),1 };
+        }
+        else {
+        star[i] = { star[i].X,(short)(star[i].Y+1) };
+        }
+    }
+}
+
+void fill_star_to_buffer()
+{
+for (int i = 0; i < scount; ++i) {
+consoleBuffer[star[i].X + screen_x * star[i].Y].Char.AsciiChar = '*';
+consoleBuffer[star[i].X + screen_x * star[i].Y].Attributes = 7;
+}
+}
+
+void Check(int x,int y)
+{
+    for (int i = 0; i < scount; ++i)
+    {
+        if(star[i].X >= x && star[i].X <= x+4 && star[i].Y == y){
+            star[i].X = rand() % screen_x ;
+            star[i].Y = 1;
+            HP--;   
+        }
+    }
+}
+
+int main()
+{
+    int i;
+    srand(time(NULL));
+    bool play = true;
+    DWORD numEvents = 0;
+    DWORD numEventsRead = 0;
+    setConsole(screen_x, screen_y);
+    setMode();
+    init_star();
+    int x = screen_x/2, y = screen_y,color = 7;
+    while (play && (HP != 0))
+    {
+        GetNumberOfConsoleInputEvents(rHnd, &numEvents);
+        if (numEvents != 0) {
+            INPUT_RECORD* eventBuffer = new INPUT_RECORD[numEvents];
+            ReadConsoleInput(rHnd, eventBuffer, numEvents, &numEventsRead);
+            for (DWORD i = 0; i < numEventsRead; ++i) {
+                if (eventBuffer[i].EventType == KEY_EVENT && eventBuffer[i].Event.KeyEvent.bKeyDown == true ) {
+                    if (eventBuffer[i].Event.KeyEvent.wVirtualKeyCode == VK_ESCAPE) {
+                        play = false;
+                    }
+                    if(eventBuffer[i].Event.KeyEvent.uChar.AsciiChar == 'c'){
+                    color = rand()%255;
+                    }
+                    }
+                else if (eventBuffer[i].EventType == MOUSE_EVENT) {
+                    int posx = eventBuffer[i].Event.MouseEvent.dwMousePosition.X;
+                    int posy = eventBuffer[i].Event.MouseEvent.dwMousePosition.Y;
+                    if (eventBuffer[i].Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED) {
+                        color = rand()%255;
+                    }
+                    else if (eventBuffer[i].Event.MouseEvent.dwEventFlags & MOUSE_MOVED) {
+                        x=posx;
+                        y=posy;
+                    }
+                }
+            }
+            delete[] eventBuffer;
+        }
+star_fall();
+clear_buffer();
+fill_star_to_buffer();
+player_buffer(x,y,color);
+Check(x,y);
+fill_buffer_to_console();
+Sleep(100);
+}
+return 0;
+}
